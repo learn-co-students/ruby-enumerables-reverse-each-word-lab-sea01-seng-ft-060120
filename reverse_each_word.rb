cf6caf0374e2cf31516310acbd3e5f22d0412afc
Carly L <carlylynnla@gmail.com>
2020-05-10 23:52:57 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,7 @@
 def reverse_each_word(string)
-  string_to_array = []
   string_to_array = string.split(" ")
-  strings_in_array_reversed = []
-  string_to_array.collect do |element|
-    strings_in_array_reversed << element.reverse
+  strings_in_array_reversed = string_to_array.collect do |element|
+    element.reverse
   end
   strings_in_array_reversed.join(" ")
 end
